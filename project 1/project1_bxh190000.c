467d5909d2b51b56bc0112d502ea5c10a5f7263c
BoYu Huang <n16064674@gs.ncku.edu.tw>
2020-09-02 15:51:53 -0500

    add wait function

--- project 1/project1_bxh190000.c
+++ project 1/project1_bxh190000.c
@@ -2,22 +2,7 @@
 // UTD id: 2021485527, NET id: bxh190000
 // Operating System Concepts project 1
 
-#include <stdio.h>
-
-main(){
-    int p = -5;
-    
-    // create 5 child process
-    for (int i = 0; i < 5; i++){
-        p = fork();
-        
-        // To avoid looping through again
-        // if the return p value is 0 (child process), break out.
-        if (p == 0){
-            break;
-        }
-    }
-    /**
+/**
      * if a program contains a call to fork( ), 
      * the execution of the program results in the execution of two processes. 
      * One process is created to start executing the program. 
@@ -30,27 +15,36 @@ main(){
      * execution to the main program. Since UNIX is a time-shared operating system, 
      * the two processes can execute concurrently.
      *
-    */
+*/
+#include <stdio.h>
+
+main(){
+    int p = -5;
+    
+    // create 5 child process
+    for (int i = 0; i < 5; i++){
+        p = fork();
+        
+        if (p == 0){
+            // To avoid looping through again
+            // if the return p value is 0 (child process), break out.
+            break;
+        }
+    }
     
     if (p == 0){
         // child process
-        
-        //printf("P value of the children is %d\n", p);
-        //printf("The PID of the children %d\n", getpid());
-        //printf("The PID of the parent %d\n", getppid());
         
         printf("I am child process number %d. Terminating now.\n", getpid()-getppid());
         // print the difference between the PID of original parent process and the PID 
         // of child process created by fork() call in the parent
     }
     else {
-        // parent process 
-        
-        //printf("P value of the current is %d\n", p);
-        //printf("The PID of the current %d\n", getpid());
-        //printf("The PID of the current's parent %d\n", getppid());
+        // parent process
         
         printf(""); // print nothing if it's the origin parent process
+        sleep(1); 
+        
     }
     
     //printf("Common part\n");
